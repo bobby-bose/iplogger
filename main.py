3d5cab44530d8e8dd398297b9910414749ae4f1e
bobby-bose <bobbykboseoffice@gmail.com>
2025-10-01 21:49:53 +0530

    gg

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/receive', methods=['POST'])
+def receive():
+    data = request.get_json()
+    message = data.get("message", "")
+    print("Received H1 content:", message)
+    return jsonify({"status": f"Message received: {message}"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
